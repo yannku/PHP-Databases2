50aaf126cb8866f6e7fd6a4f4364a8cffbb8dfcf
yannku <Yannick.Farrugia.a104041@mcast.edu.mt>
2018-04-18 02:49:10 +0200

    latest update

--- Testing/js/website.js
+++ Testing/js/website.js
@@ -1,22 +1,15 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
+$('.dropdown-btn').each(function(){
+    function toggleDropdown(e)
+    {
+        var display = $(e).hasClass('active') ? 'block' : 'none';
+        var toggle = $(e).attr('data-toggle');
+        $(toggle).css('display', display);
     }
-  });
-}
+
+    $(this).click(function(){
+        $(this).toggleClass('active');
+        toggleDropdown(this);
+    });
+
+    toggleDropdown(this);
+});
