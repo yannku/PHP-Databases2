53abf632ef71f57b3d7e0d8eed13589cb31446d3
lorellaaquilina <lorella.aquilina.a104010@mcast.edu.mt>
2018-06-01 12:01:55 +0200

    Forms

---
+++ Main/application/views/forms.php
@@ -0,0 +1,45 @@
+<div class="row justify-content-center">
+    <div class="col-sm-6">
+        <h1>Forms</h1>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-sm-4">
+        <h3>Artistic work release form</h3>
+    </div>
+    <div class="col-sm-2">
+        <button type="button" name="button"><a href="<?=base_url('/ViewerJS/#../uploads/pdf/Artistic-Work-Release-Form.pdf')?>">View</a></button>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-sm-4">
+        <h3>Children release form</h3>
+    </div>
+    <div class="col-sm-2">
+        <button type="button" name="button"><a href="<?=base_url('/ViewerJS/#../uploads/pdf/Children-Release-Form.pdf')?>">View</a></button>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-sm-4">
+        <h3>Location release form</h3>
+    </div>
+    <div class="col-sm-2">
+        <button type="button" name="button"><a href="<?=base_url('/ViewerJS/#../uploads/pdf/Location-Release-Form.pdf')?>">View</a></button>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-sm-4">
+        <h3>Talent release form</h3>
+    </div>
+    <div class="col-sm-2">
+        <button type="button" name="button"><a href="<?=base_url('/ViewerJS/#../uploads/pdf/Talent-Release-Form.pdf')?>">View</a></button>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-sm-4">
+        <h3>Other release forms</h3>
+    </div>
+    <div class="col-sm-2">
+        <button type="button" name="button"><?=anchor('http://www.mcast.edu.mt/180', 'View others'); ?></button>
+    </div>
+</div>
